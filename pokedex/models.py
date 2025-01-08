a01b42232bcdf000e883b4fd7f06783eddf2d71e
Elizabeth Mites <elizabethmites2703@gmail.com>
2025-01-08 16:45:49 -0500

    Laboratorio 8

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    type = models.CharField(max_length=30, null=False)
+    weight = models.DecimalField(decimal_places=4, max_digits=6)
+    height = models.DecimalField(decimal_places=4, max_digits=6)
+
+    def __str__(self):
+        return self.name
+
+# Creacion de la clase Trainer
+class Trainer(models.Model):
+    first_name = models.CharField(max_length=30, null=False)
+    last_name = models.CharField(max_length=30, null=False)
+    birth_date = models.DateField()
+    level = models.IntegerField(default=1)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
