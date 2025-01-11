97bc54ef9b3bec1bfd00999be6adec3d128925fc
Elizabeth Mites <elizabethmites2703@gmail.com>
2025-01-10 22:33:19 -0500

    Laboratorio 9

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,16 +1,6 @@
 from django.db import models
-
-# Create your models here.
-class Pokemon(models.Model):
-    name = models.CharField(max_length=30, null=False)
-    type = models.CharField(max_length=30, null=False)
-    weight = models.DecimalField(decimal_places=4, max_digits=6)
-    height = models.DecimalField(decimal_places=4, max_digits=6)
-
-    def __str__(self):
-        return self.name
-
 # Creacion de la clase Trainer
+
 class Trainer(models.Model):
     first_name = models.CharField(max_length=30, null=False)
     last_name = models.CharField(max_length=30, null=False)
@@ -19,3 +9,24 @@ class Trainer(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+    
+# Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    POKEMON_TYPES = {
+        ('A', 'Agua'),
+        ('F', 'Fuego'),
+        ('T', 'Tierra'),
+        ('P', 'Planta'),
+        ('E', 'Eléctrico'),
+        ('L', 'Lagartija'),   
+    }
+    type =models.CharField(max_length=30, choices= POKEMON_TYPES, null=False)
+    weight = models.DecimalField(decimal_places=4, max_digits=6)
+    height = models.DecimalField(decimal_places=4, max_digits=6)
+    trainer = models.ForeignKey(Trainer, on_delete=models.SET_NULL, null=True)
+    picture = models.ImageField(upload_to="pokemon_images")
+    
+
+    def __str__(self):
+        return self.name
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Pokemon
+from .models import Pokemon, Trainer
+from django.shortcuts import redirect, render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from pokedex.forms import PokemonForm
 
 def index(request):
     pokemons = Pokemon.objects.all()
+    trainers = Trainer.objects.all()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render({'pokemons': pokemons}, request))
+    return HttpResponse(template.render({
+        'pokemons': pokemons,
+        'trainers' : trainers
+    }, 
+    request))
 
 def pokemon(request, pokemon_id):
     pokemon = Pokemon.objects.get(id = pokemon_id)
@@ -14,3 +23,50 @@ def pokemon(request, pokemon_id):
         'pokemon': pokemon
     }
     return HttpResponse(template.render(context, request))
+
+def trainer_details(request, trainer_id):
+    trainer = Trainer.objects.get(id = trainer_id)
+    template = loader.get_template('display_trainer.html')
+    context = {
+        'trainer': trainer
+    }
+    return HttpResponse(template.render(context, request))
+
+@login_required
+def add_pokemon(request):
+    if request.method == "POST":
+        form = PokemonForm(request.POST, request.FILES)  
+        if form.is_valid():
+             form.save()
+             return redirect('pokedex:index')
+    else:
+            form = PokemonForm()
+    return render(request, 'pokemon_form.html', {'form': form})
+
+@login_required
+def edit_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id = pokemon_id)
+    if request.method == "POST":
+        form = PokemonForm(request.POST, request.FILES, instance=pokemon)  
+        if form.is_valid():
+             form.save()
+             return redirect('pokedex:index')
+    else:
+            form = PokemonForm(instance=pokemon)
+    return render(request, 'pokemon_form.html', {'form': form})
+
+@login_required
+def delete_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id = pokemon_id)
+    pokemon.delete()
+    return redirect('pokedex:index')
+
+class CustomLoginView(LoginView):
+     template_name = "login_form.html"
+    
+
+    
+
+
+
+    
